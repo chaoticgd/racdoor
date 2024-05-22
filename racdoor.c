cc4e1b00ab20cae6bb42d82f9138507618168c24
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2024-05-22 19:53:46 +0100

    Extend to shellcode to clean up after itself
    This way the game can continue to run after the entry point returns
    instead of immediately crashing.

--- racdoor.c
+++ racdoor.c
@@ -12,6 +12,7 @@ typedef struct {
 	uint32_t HelpDataGadgets;
 	uint32_t HelpDataMisc;
 	uint32_t HelpLog;
+	uint32_t HelpLogPos;
 	uint32_t GetGadgetEvent_jal_FastMemCpy;
 } Addresses;
 
@@ -32,6 +33,7 @@ Build builds[] = {
 			.HelpDataGadgets                    = 0x001f7660,
 			.HelpDataMisc                       = 0x001f7750,
 			.HelpLog                            = 0x001f7810,
+			.HelpLogPos                         = 0x0021defc,
 			.GetGadgetEvent_jal_FastMemCpy      = 0x006c48c8
 		}
 	}
@@ -63,6 +65,7 @@ enum SaveBlockType {
 	BLOCK_HelpDataMessages = 16,
 	BLOCK_HelpDataMisc = 17,
 	BLOCK_HelpDataGadgets = 18,
+	BLOCK_HelpLog = 1010,
 	BLOCK_HelpLogPos = 1011,
 	BLOCK_g_HeroGadgetBox = 7008
 };
@@ -162,11 +165,24 @@ SaveBlock* lookup_block(SaveSlotBlockList* list, int32_t type);
 #define MIPS_ZERO 0
 #define MIPS_A0 4
 #define MIPS_A1 5
+#define MIPS_A2 6
+#define MIPS_A3 7
+#define MIPS_T0 8
+#define MIPS_T1 9
+#define MIPS_T2 10
+#define MIPS_T3 11
+#define MIPS_SP 29
 
-#define MIPS_LUI(dest, immediate) ((immediate) & 0xffff) | ((dest) << 16) | (0b001111 << 26)
-#define MIPS_ADDIU(dest, source, immediate) (((immediate) & 0xffff) | ((dest) << 16) | ((source) << 21) | (0b001001 << 26))
-#define MIPS_JAL(target) ((target >> 2) & 0x3ffffff) | (0b000011 << 26)
 #define MIPS_NOP() 0
+#define MIPS_ADD(dest, rs, rt) 0b100000 | ((dest) << 11) | ((rt) << 16) | ((rs) << 21) | (0b000000 << 26)
+#define MIPS_J(target) ((target >> 2) & 0x3ffffff) | (0b000010 << 26)
+#define MIPS_JAL(target) ((target >> 2) & 0x3ffffff) | (0b000011 << 26)
+#define MIPS_BEQ(rs, rt, offset) ((offset) & 0xffff) | ((rt) << 16) | ((rs) << 21) | (0b100 << 26)
+#define MIPS_ADDIU(dest, source, immediate) (((immediate) & 0xffff) | ((dest) << 16) | ((source) << 21) | (0b001001 << 26))
+#define MIPS_LUI(dest, immediate) ((immediate) & 0xffff) | ((dest) << 16) | (0b001111 << 26)
+#define MIPS_LW(dest, offset, base) (offset & 0xffff) | ((dest) << 16) | ((base) << 21) | (0b100011 << 26)
+#define MIPS_LBU(dest, offset, base) (offset & 0xffff) | ((dest) << 16) | ((base) << 21) | (0b100100 << 26)
+#define MIPS_SW(src, offset, base) (offset & 0xffff) | ((src) << 16) | ((base) << 21) | (0b101011 << 26)
 
 void mutate_save_game(SaveSlot* save, Buffer elf, Addresses* addresses);
 
@@ -190,10 +206,18 @@ int main(int argc, char** argv)
 	write_file(output_save_path, file);
 }
 
+// (maxlevel+1)*sizeof(StackFixup) == 75*56 == 4200 == sizeof(HelpLog)
+typedef struct {
+	uint8_t offset[11];
+	uint8_t terminator;
+	uint32_t data[11];
+} StackFixup;
+
 void mutate_save_game(SaveSlot* save, Buffer elf, Addresses* addresses)
 {
 	SaveBlock* help_data_messages = lookup_block(&save->game, BLOCK_HelpDataMessages);
-	SaveBlock* help_data_misc = lookup_block(&save->game, BLOCK_HelpDataMisc);
+	SaveBlock* help_data_gadgets = lookup_block(&save->game, BLOCK_HelpDataGadgets);
+	SaveBlock* help_log = lookup_block(&save->game, BLOCK_HelpLog);
 	SaveBlock* help_log_pos = lookup_block(&save->game, BLOCK_HelpLogPos);
 	SaveBlock* gadget_box = lookup_block(&save->game, BLOCK_g_HeroGadgetBox);
 	
@@ -207,21 +231,107 @@ void mutate_save_game(SaveSlot* save, Buffer elf, Addresses* addresses)
 	uint32_t offset = addresses->GetGadgetEvent_jal_FastMemCpy + 6 - addresses->HelpLog;
 	*(uint32_t*) help_log_pos->data = (offset >> 1) | 0x80000000;
 	
+	uint32_t memcpy_dest_addr = 0x01fffa60;
+	uint32_t memcpy_src_addr = 0x001d4090;
+	uint32_t gadget_box_addr = addresses->g_HeroGadgetBox;
+	uint32_t ofs_in_gadget_box_for_memcpy = memcpy_src_addr - gadget_box_addr;
+	uint32_t stack_buffer_ra_ofs = 0x78;
+	uint32_t ofs_in_gadget_box_for_ra = ofs_in_gadget_box_for_memcpy + stack_buffer_ra_ofs;
+	
 	// Overwrite the return address with the address of the HelpDataMisc global.
 	CHECK((gadget_box->size % 4) == 0, "Invalid g_HeroGadgetBox block.\n");
-	for(uint32_t i = 0x30 + 0x50; i < 0x0a30; i += 4)
-		*(uint32_t*) &gadget_box->data[i] = i;
-	*(uint32_t*) &gadget_box->data[0xa8] = addresses->HelpDataMisc;
+	
+	// Clear all the gadget events, including the head pointer. This is to make
+	// the data that gets written onto the stack more predictable.
+	*(uint32_t*) &gadget_box->data[0x2c] = 0;
+	for(int32_t offset = 0; offset < 0x0a00; offset += 4)
+		*(uint32_t*) &gadget_box->data[ofs_in_gadget_box_for_memcpy + offset] = offset;
+	
+	// Set the gadget type field of all the gadget events to 0xff to mark them
+	// as free. If we don't do this Ratchet won't be able to shoot.
+	for(int32_t offset = 0x30; offset < 0x0a30; offset += 0x50)
+		gadget_box->data[offset + 0x2] = 0xff;
+	
+	*(uint32_t*) &gadget_box->data[ofs_in_gadget_box_for_ra] = addresses->HelpDataGadgets;
+	
+	//uint32_t ra_address_on_stack = 0x1fffb10;
+	//uint32_t ra_address_2 = 0x67d410;
+	//uint32_t write_ofs = ofs_in_gadget_box_for_memcpy + (ra_address_on_stack - memcpy_dest_addr);
+	//*(uint32_t*) &gadget_box->data[write_ofs] = ra_address_2;
+	
+	StackFixup fixup;
+	memset(&fixup, 0xff, sizeof(fixup));
+	fixup.offset[0] = 0xb0;
+	fixup.data[0] = 0x67d410;
+	fixup.offset[1] = 0xb8;
+	fixup.data[1] = 0x67d264;
+	fixup.offset[2] = 0xcc;
+	fixup.data[2] = 0x6abff8;
+	fixup.offset[3] = 0x80;
+	fixup.data[3] = 0x368f60;
+	fixup.offset[4] = 0x88;
+	fixup.data[4] = 0x368f60;
+	fixup.offset[5] = 0x48;
+	fixup.data[5] = 0x368f60;
+	fixup.offset[5] = 0x58;
+	fixup.data[5] = 0x368f60;
+	for(int32_t level = 0; level < 2; level++)
+		memcpy(help_log->data + level * sizeof(StackFixup), &fixup, sizeof(fixup));
+	
+	uint32_t* shellcode = (uint32_t*) help_data_gadgets->data;
 	
 	// Unpack the payload and jump to it.
-	uint32_t* shellcode = (uint32_t*) help_data_misc->data;
 	*shellcode++ = MIPS_LUI(MIPS_A0, addresses->HelpDataMessages >> 16);
-	*shellcode++ = MIPS_ADDIU(MIPS_A0, MIPS_A0, addresses->HelpDataMessages & 0xffff);
+	*shellcode++ = MIPS_ADDIU(MIPS_A0, MIPS_A0, addresses->HelpDataMessages);
 	*shellcode++ = MIPS_LUI(MIPS_A1, 0xffff);
 	*shellcode++ = MIPS_ADDIU(MIPS_A1, MIPS_A1, 0xffff);
 	*shellcode++ = MIPS_JAL(addresses->unpackbuff);
 	*shellcode++ = MIPS_NOP(); // Delay slot.
+	// flush cache here
 	*shellcode++ = MIPS_JAL(payload_entry);
+	*shellcode++ = MIPS_NOP(); // Delay slot.
+	
+	// Clean up the stack.
+	uint32_t expected_sp = 0x01fffae0;
+	*shellcode++ = MIPS_ADDIU(MIPS_A0, MIPS_SP, memcpy_dest_addr - expected_sp);
+	*shellcode++ = MIPS_LUI(MIPS_A1, addresses->HelpLog >> 16);
+	*shellcode++ = MIPS_ADDIU(MIPS_A1, MIPS_A1, addresses->HelpLog);
+	*shellcode++ = MIPS_ADDIU(MIPS_A2, MIPS_A1, 12);
+	*shellcode++ = MIPS_ADDIU(MIPS_T0, MIPS_ZERO, 0xff);
+	// loop begin
+	*shellcode++ = MIPS_LBU(MIPS_T1, 0, MIPS_A1);
+	*shellcode++ = MIPS_BEQ(MIPS_T1, MIPS_T0, 8);
+	*shellcode++ = MIPS_NOP(); // Delay slot.
+	*shellcode++ = MIPS_LW(MIPS_T2, 0, MIPS_A2);
+	
+	*shellcode++ = MIPS_ADD(MIPS_T3, MIPS_A0, MIPS_T1);
+	*shellcode++ = MIPS_SW(MIPS_T2, 0, MIPS_T3);
+	
+	*shellcode++ = MIPS_ADDIU(MIPS_A1, MIPS_A1, 1);
+	*shellcode++ = MIPS_ADDIU(MIPS_A2, MIPS_A2, 4);
+	
+	*shellcode++ = MIPS_BEQ(MIPS_ZERO, MIPS_ZERO, -9);
+	*shellcode++ = MIPS_NOP(); // Delay slot.
+	// loop end
+	
+	// Clean up HelpLogPos so we never call into this shellcode again.
+	*shellcode++ = MIPS_LUI(MIPS_T0, addresses->HelpLogPos >> 16);
+	*shellcode++ = MIPS_ADDIU(MIPS_T0, MIPS_T0, addresses->HelpLogPos);
+	*shellcode++ = MIPS_SW(MIPS_ZERO, 0, MIPS_T0);
+	
+	// Clean up the corrupted instructions in GadgetBox::GetGadgetEvent.
+	*shellcode++ = MIPS_LUI(MIPS_A0, (addresses->GetGadgetEvent_jal_FastMemCpy + 4) >> 16);
+	*shellcode++ = MIPS_ADDIU(MIPS_A0, MIPS_A0, addresses->GetGadgetEvent_jal_FastMemCpy + 4);
+	*shellcode++ = MIPS_LUI(MIPS_T0, 0x2406);
+	*shellcode++ = MIPS_ADDIU(MIPS_T0, MIPS_T0, 0x0050);
+	*shellcode++ = MIPS_SW(MIPS_T0, 0, MIPS_A0);
+	*shellcode++ = MIPS_ADDIU(MIPS_A0, MIPS_A0, 4);
+	*shellcode++ = MIPS_LUI(MIPS_T0, 0xa2b4);
+	*shellcode++ = MIPS_ADDIU(MIPS_T0, MIPS_T0, 0x0002);
+	*shellcode++ = MIPS_SW(MIPS_T0, 0, MIPS_A0);
+	
+	// Jump back to the game.
+	*shellcode++ = MIPS_J(0x0067cca0);
 	*shellcode++ = MIPS_NOP(); // Delay slot.
 }
 
@@ -325,6 +435,10 @@ SaveBlock* lookup_block(SaveSlotBlockList* list, int32_t type)
 
 uint32_t convert_elf(char* output, int32_t output_size, Buffer input)
 {
+	int32_t size_left = output_size;
+	
+	printf("%.2fkb total\n", output_size / 1024.f);
+	
 	ElfFileHeader* elf_header = buffer_get(input, 0, sizeof(ElfFileHeader), "ELF file header");
 	CHECK(elf_header->ident_magic == 0x464c457f, "ELF file has bad magic number.\n");
 	CHECK(elf_header->ident_class == 1, "ELF file isn't 32 bit.\n");
@@ -336,7 +450,7 @@ uint32_t convert_elf(char* output, int32_t output_size, Buffer input)
 		ElfProgramHeader* program_header = buffer_get(input, program_header_offset, sizeof(ElfProgramHeader), "ELF program header");
 		
 		int32_t size = align32(sizeof(RatchetSectionHeader) + program_header->filesz, 4);
-		CHECK(size >= 0 && output_size >= size, "ELF too big!\n");
+		CHECK(size >= 0 && size_left >= size, "ELF too big!\n");
 		
 		RatchetSectionHeader* output_header = (RatchetSectionHeader*) output;
 		output_header->address = program_header->vaddr;
@@ -348,11 +462,14 @@ uint32_t convert_elf(char* output, int32_t output_size, Buffer input)
 		memcpy(output + sizeof(RatchetSectionHeader), segment, program_header->filesz);
 		
 		output += size;
-		output_size -= size;
+		size_left -= size;
 	}
 	
-	CHECK(output_size >= sizeof(RatchetSectionHeader), "ELF too big!\n");
+	CHECK(size_left >= sizeof(RatchetSectionHeader), "ELF too big!\n");
 	memset(output, 0, sizeof(RatchetSectionHeader));
+	
+	printf("%.2fkb used\n", (output_size - size_left) / 1024.f);
+	printf("%.2fkb left\n", size_left / 1024.f);
 	
 	return elf_header->entry;
 }
