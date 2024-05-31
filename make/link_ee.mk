15bde8b6725255f22f96f0ffbf57d829865f99ef
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2024-05-31 18:35:29 +0100

    Add some missing makefile dependencies

--- make/link_ee.mk
+++ make/link_ee.mk
@@ -5,10 +5,10 @@ SERIAL_FROM_TABLE = $(patsubst $(BIN).%.t,%,$@)
 TABLE_FROM_BIN = $(patsubst $(BIN).%.elf,$(BIN).%.t,$@)
 
 .PRECIOUS: $(BIN).%.t
-$(BIN).%.t: $(OBJS) $(PROJECTDIR)/tables/%.csv
+$(BIN).%.t: $(OBJS) $(PROJECTDIR)/tables/%.csv $(TBLGEN)
 	$(TBLGEN) $(OBJS) -t $(PROJECTDIR)/tables/$(SERIAL_FROM_TABLE).csv -o $@ -v
 
-$(BIN).%.elf: $(OBJS) $(BIN).%.t
+$(BIN).%.elf: $(OBJS) $(BIN).%.t $(PROJECTDIR)/linkfile.ld
 	$(CC) $(OBJS) $(TABLE_FROM_BIN) -o $@ $(LINKFLAGS)
 
 .PHONY: $(BIN)
