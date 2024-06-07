c1c56b875b597db2b462408b24e019b1cdb0eede
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2024-06-07 22:16:36 +0100

    Simplify link_ee.mk makefile

--- make/link_ee.mk
+++ make/link_ee.mk
@@ -3,29 +3,24 @@ ELFS := $(patsubst %,$(BIN).%.elf,$(basename $(SERIALS)))
 RDXS := $(patsubst %,$(BIN).%.rdx,$(basename $(SERIALS)))
 MAPS := $(patsubst %,$(BIN).%.map,$(basename $(SERIALS)))
 
-SERIAL_FROM_TABLE = $(patsubst $(BIN).%.tbl,%,$@)
-SERIAL_FROM_BIN = $(patsubst $(BIN).%.elf,%,$@)
-TABLE_FROM_BIN = $(patsubst $(BIN).%.elf,$(BIN).%.tbl,$@)
-BIN_FROM_RDX = $(patsubst $(BIN).%.rdx,$(BIN).%.elf,$@)
-
 # Generate an object file containing .symtab symbols for all the core symbols
 # loaded from the provided CSV file and some custom sections for all of the
 # overlay symbols.
 .PRECIOUS: $(BIN).%.tbl
 $(BIN).%.tbl: $(TBLGEN) $(OBJS) $(PROJECTDIR)/tables/output/%.csv
-	$(TBLGEN) $(OBJS) -t $(PROJECTDIR)/tables/output/$(SERIAL_FROM_TABLE).csv -s $* -o $@ -v
+	$(TBLGEN) $(OBJS) -t $(PROJECTDIR)/tables/output/$*.csv -s $* -o $@ -v
 
 # Perform partial linking with the -r option passed to GNU ld. This will combine
 # together the sections from the object files given as input but it will not
 # apply relocations for external symbols.
 .PRECIOUS: $(BIN).%.elf
 $(BIN).%.elf: $(OBJS) $(BIN).%.tbl $(PROJECTDIR)/linkfile.ld
-	$(CC) $(OBJS) $(TABLE_FROM_BIN) -o $@ -nostdlib -Wl,-r,-T,$(PROJECTDIR)/linkfile.ld,-Map,$(BIN).$(SERIAL_FROM_BIN).map
+	$(CC) $(OBJS) $(BIN).$*.tbl -o $@ -nostdlib -Wl,-r,-T,$(PROJECTDIR)/linkfile.ld,-Map,$(BIN).$*.map
 
 # Apply relocations for static symbols and copy relocations for dynamic symbols
 # into the .racdoor.relocs section so that they can be applied at runtime.
 $(BIN).%.rdx: $(RDXGEN) $(BIN).%.elf
-	$(RDXGEN) $(BIN_FROM_RDX) $@
+	$(RDXGEN) $(BIN).$*.elf $@
 
 .PHONY: $(BIN)
 $(BIN): $(RDXS)
