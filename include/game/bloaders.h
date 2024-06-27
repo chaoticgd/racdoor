a09eb772769214a6a9a395a1dd3d9de805e3cb20
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2024-06-27 02:37:49 +0100

    Add debug menu sample

---
+++ include/game/bloaders.h
@@ -0,0 +1,6 @@
+#ifndef _GAME_BLOADERS_H
+#define _GAME_BLOADERS_H
+
+void LoadDebugFont();
+
+#endif
---
+++ samples/debugmenu/debugmenu.c
@@ -0,0 +1,28 @@
+#include <game/bloaders.h>
+#include <game/gameglobals.h>
+
+#include <racdoor/hook.h>
+#include <racdoor/module.h>
+
+void PauseGame(int screen);
+void pause_game_thunk(int screen);
+FuncHook pause_game_hook = {};
+TRAMPOLINE(pause_game_trampoline, void);
+
+void debugmenu_load()
+{
+	install_hook(&pause_game_hook, PauseGame, pause_game_thunk, pause_game_trampoline);
+}
+
+void pause_game_thunk(int screen)
+{
+	pause_game_trampoline(screen);
+	
+	if (screen == 10)
+	{
+		gameMode = GAME_MODE_DEBUG;
+		LoadDebugFont();
+	}
+}
+
+MODULE(debugmenu_load, NULL);
