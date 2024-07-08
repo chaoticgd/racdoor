de2bd2f40778436a175ceb3c2a12e9526080b45f
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2024-07-08 18:20:58 +0100

    Load the debug font each time the debug menu is opened

--- samples/debugmenu/debugmenu.c
+++ samples/debugmenu/debugmenu.c
@@ -8,12 +8,14 @@ void PauseGame(int screen);
 void pause_game_thunk(int screen);
 AUTO_HOOK(PauseGame, pause_game_thunk, pause_game_trampoline, void);
 
-MODULE_LOAD_FUNC(LoadDebugFont);
-
 void pause_game_thunk(int screen)
 {
 	pause_game_trampoline(screen);
 	
 	if (screen == 10)
+	{
 		gameMode = GAME_MODE_DEBUG;
+		
+		LoadDebugFont();
+	}
 }
